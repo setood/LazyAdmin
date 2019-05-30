6640364d2eede2d6f60070b5bda845dfb62d6b7c
Vsevolod <sevik_sm@mail.ru>
2019-05-31 00:05:53 +0600

    init

---
+++ LazyAdmin/Ladmin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace LazyAdmin
+{
+    public class Ladmin
+    {
+        public DataTable Execute(string queryText)
+        {
+            throw new System.NotImplementedException();
+        }
+        public DataTable Execute(string queryText, DataTable importData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
